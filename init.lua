a65c9f3e90e190a18acc47fc54a412b752efc3f6
Diego Rojas <devup2332@gmail.com>
2022-03-03 00:38:35 -0500

    master: Initial Commit

---
+++ init.lua
@@ -0,0 +1,48 @@
+-- Plugins and settings
+require 'diego.options'
+require 'diego.keymaps'
+require 'diego.plugins'
+
+
+--Themes
+require 'diego.colorscheme'
+
+--Impatient 
+require 'diego.impatient'
+
+--Alpha 
+require 'diego.alpha'
+
+--Telescope 
+require 'diego.telescope'
+
+--Whichkey 
+require 'diego.whichkey'
+
+--Autopairs 
+require 'diego.autopairs'
+
+--Nvim-tree-explorer 
+require 'diego.nvim-tree'
+
+--LSP
+require 'diego.lsp'
+
+--CMP
+require 'diego.cmp'
+
+--Null-ls
+require 'diego.lsp.null-ls'
+
+--Indent
+require 'diego.indent'
+
+--BufferLine
+require 'diego.bufferline'
+
+--Treeseeter
+require 'diego.treeseter'
+
+--Git
+require 'diego.gitsigns'
+
---
+++ lua/diego/telescope.lua
@@ -0,0 +1,30 @@
+local status_ok, telescope = pcall(require,'telescope')
+
+if not status_ok then
+  return
+end
+
+telescope.setup{
+  defaults = {
+    file_ignore_patterns = {"node_modules",".git",".next"},
+    prompt_prefix = ' ',
+    selection_caret = '  ',
+    mappings = {
+      i = {
+        ["<C-h>"] = "which_key"
+      }
+    }
+  },
+  pickers = {
+    find_files = {
+      previewer = false
+    },
+  },
+  extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+    -- please take a look at the readme of the extension you want to configure
+  }
+}
