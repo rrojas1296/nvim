609ff1e6424841998b1d13279d3dccd712d5702d
Diego Rojas <devup2332@gmail.com>
2023-07-17 14:55:24 -0500

    master: Changed Icons

--- lua/diego/alpha.lua
+++ lua/diego/alpha.lua
@@ -5,11 +5,11 @@ end
 local dashboard = require("alpha.themes.dashboard")
 
 dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find file", ":Telescope find_files hidden=true theme=dropdown<CR>"),
-	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("r", "  Recently used files", ":Telescope oldfiles hidden=true<CR>"),
+	dashboard.button("f", "  Find file", ":Telescope find_files hidden=true theme=dropdown<CR>"),
+	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("r", "  Recently used files", ":Telescope oldfiles hidden=true<CR>"),
 	dashboard.button("t", "  Find text", ":Telescope live_grep hidden=true<CR>"),
-	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+	dashboard.button("q", "󰗼  Quit Neovim", ":qa<CR>"),
 }
 
 local function footer()
