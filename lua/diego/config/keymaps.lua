84c5a32351e09f514df3573c1e5fad8b87638f5e
Diego Rojas <devup2332@gmail.com>
2024-10-17 00:43:09 -0500

    master: Changed shortcuts

--- lua/diego/config/keymaps.lua
+++ lua/diego/config/keymaps.lua
@@ -43,8 +43,8 @@ keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("n", "<A-j>", ":m .+1<CR>==", opts)
+keymap("n", "<A-k>", ":m .-2<CR>==", opts)
 
 -- Visual Block --
 -- Move text up and down
