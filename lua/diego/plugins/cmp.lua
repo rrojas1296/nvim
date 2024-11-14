df265b818661cd606cf3e7b0b592c311fcdbb9de
Diego Rojas <devup2332@gmail.com>
2024-11-14 10:31:29 -0500

    masteR: Edited autopairs and cmp  plugin

--- lua/diego/plugins/cmp.lua
+++ lua/diego/plugins/cmp.lua
@@ -5,15 +5,29 @@ return {
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-cmdline',
+
     "windwp/nvim-autopairs",
     "onsails/lspkind.nvim",
+
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip',
+
+    'hrsh7th/cmp-vsnip',
+    'hrsh7th/vim-vsnip',
+
+    'SirVer/ultisnips',
+    'quangnguyen30192/cmp-nvim-ultisnips',
+
+    'dcampos/nvim-snippy',
+    'dcampos/cmp-snippy',
+
+    "mlaursen/vim-react-snippets",
   },
+  opts = function()
+    -- require("vim-react-snippets").lazy_load()
+  end,
   config = function()
     local cmp = require('cmp')
-
-    local lspkind = require('lspkind')
-
-    local autopairs = require('nvim-autopairs')
 
     local signs = {
       Error = "",
@@ -27,9 +41,45 @@ return {
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
 
-    autopairs.setup()
+    local kind_icons = {
+      Text = "",
+      Method = "󰆧",
+      Function = "󰊕",
+      Constructor = "",
+      Field = "󰇽",
+      Variable = "󰂡",
+      Class = "󰠱",
+      Interface = "",
+      Module = "",
+      Property = "󰜢",
+      Unit = "",
+      Value = "󰎠",
+      Enum = "",
+      Keyword = "󰌋",
+      Snippet = "",
+      Color = "󰏘",
+      File = "󰈙",
+      Reference = "",
+      Folder = "󰉋",
+      EnumMember = "",
+      Constant = "󰏿",
+      Struct = "",
+      Event = "",
+      Operator = "󰆕",
+      TypeParameter = "󰅲",
+    }
 
     cmp.setup({
+      snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+          -- vim.fn["vsnip#anonymous"](args.body)        -- For `vsnip` users.
+          -- require('luasnip').lsp_expand(args.body)    -- For `luasnip` users.
+          require('snippy').expand_snippet(args.body) -- For `snippy` users.
+          -- vim.fn["UltiSnips#Anon"](args.body)         -- For `ultisnips` users.
+          -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+        end,
+      },
       window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
@@ -45,20 +95,29 @@ return {
       formatting = {
         fields = { "kind", "abbr", "menu" },
         expandable_indicator = true,
-        format = lspkind.cmp_format({
-          mode = "symbol_text",
-          menu = ({
+        format = function(entry, vim_item)
+          -- Kind icons
+          vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
+          -- Source
+          vim_item.menu = ({
             buffer = "[Buffer]",
-            nvim_lsp = "[LSP]",
+            path = "[Path]",
             luasnip = "[LuaSnip]",
             nvim_lua = "[Lua]",
-            latex_symbols = "[Latex]",
-          })
-        }),
+            latex_symbols = "[LaTeX]",
+            ultisnips = "[Ultisnips]",
+            nvim_lsp = "[LSP]",
+          })[entry.source.name]
+          return vim_item
+        end
       },
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
+        { name = 'vsnip' },
         { name = 'path' },
+        { name = 'luasnip' },   -- For luasnip users.
+        { name = 'ultisnips' }, -- For ultisnips users.
+        { name = 'snippy' },
       }, {
         { name = 'buffer' },
       }),
