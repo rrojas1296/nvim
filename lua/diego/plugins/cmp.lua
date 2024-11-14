e71a9b592c49f966628a348e2ba9fed0cf29eb0b
diego rojas <devup2332@gmail.com>
2024-11-14 18:40:04 -0500

    master: Edited cmp snippets

--- lua/diego/plugins/cmp.lua
+++ lua/diego/plugins/cmp.lua
@@ -15,9 +15,6 @@ return {
     'hrsh7th/cmp-vsnip',
     'hrsh7th/vim-vsnip',
 
-    'SirVer/ultisnips',
-    'quangnguyen30192/cmp-nvim-ultisnips',
-
     'dcampos/nvim-snippy',
     'dcampos/cmp-snippy',
 
@@ -73,9 +70,9 @@ return {
       snippet = {
         -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-          -- vim.fn["vsnip#anonymous"](args.body)        -- For `vsnip` users.
+          vim.fn["vsnip#anonymous"](args.body)        -- For `vsnip` users.
           -- require('luasnip').lsp_expand(args.body)    -- For `luasnip` users.
-          require('snippy').expand_snippet(args.body) -- For `snippy` users.
+          -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
           -- vim.fn["UltiSnips#Anon"](args.body)         -- For `ultisnips` users.
           -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
         end,
@@ -115,9 +112,9 @@ return {
         { name = 'nvim_lsp' },
         { name = 'vsnip' },
         { name = 'path' },
-        { name = 'luasnip' },   -- For luasnip users.
-        { name = 'ultisnips' }, -- For ultisnips users.
-        { name = 'snippy' },
+        -- { name = 'luasnip' },   -- For luasnip users.
+        -- { name = 'ultisnips' }, -- For ultisnips users.
+        -- { name = 'snippy' },
       }, {
         { name = 'buffer' },
       }),
