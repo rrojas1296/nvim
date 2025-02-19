b6f866416792d8eedee90a70ebe24f1f7b2d3d42
Diego Rojas <devup2332@gmail.com>
2025-02-19 15:21:18 -0500

    master: Changed theme

--- lua/diego/plugins/theme.lua
+++ lua/diego/plugins/theme.lua
@@ -1,11 +1,13 @@
 return {
-  "folke/tokyonight.nvim",
+  "catppuccin/nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    local theme = require('tokyonight')
+    local theme = require('catppuccin')
     local colorscheme = vim.cmd.colorscheme
-    theme.setup()
-    colorscheme "tokyonight-night"
+    theme.setup({
+      flavour = "mocha"
+    })
+    colorscheme "catppuccin"
   end
 }
