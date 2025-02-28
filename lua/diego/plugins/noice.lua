30ee0d1516ab100c80afcc6a42e9396fa2087b5e
Diego Rojas <devup2332@gmail.com>
2025-02-27 20:21:53 -0500

    master: Changed notify bg

--- lua/diego/plugins/noice.lua
+++ lua/diego/plugins/noice.lua
@@ -4,12 +4,16 @@ return {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
     -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
+      -- `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
     "rcarriga/nvim-notify",
   },
   config = function()
     local noice = require('noice')
+    local notify = require('notify')
     noice.setup()
+    notify.setup({
+      background_colour = "#000000"
+    })
   end
 }
