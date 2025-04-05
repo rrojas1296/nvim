abe81633c5fb39daa3af53b747f7e8366e4e1e15
Diego Rojas <devup2332@gmail.com>
2025-04-05 08:32:01 -0500

    master: Edited v1

--- lua/diego/plugins/lspsaga.lua
+++ lua/diego/plugins/lspsaga.lua
@@ -1,29 +1,29 @@
 return {
-  'nvimdev/lspsaga.nvim',
-  after = 'nvim-lspconfig',
-  config = function()
-    local status_ok, lspsaga = pcall(require, 'lspsaga')
-
-    if not status_ok then
-      return
-    end
-    local keymap = vim.keymap.set
-
-
-    lspsaga.setup({
-      layout = 'float',
-      finder = {
-        max_height = 0.6,
-        keys = {
-          toggle_or_open = '<cr>',
-          quit = '<esc>',
-        },
-      },
-    })
-    local opts = { noremap = true, silent = true }
-
-    keymap("n", "gr", "<cmd>Lspsaga finder ref<CR>", opts)
-    keymap('n', 'gd', "<cmd>Lspsaga goto_definition<CR>", opts)
-    keymap('n', 'K', "<cmd>Lspsaga hover_doc<CR>", opts)
-  end
+  -- 'nvimdev/lspsaga.nvim',
+  -- after = 'nvim-lspconfig',
+  -- config = function()
+  --   local status_ok, lspsaga = pcall(require, 'lspsaga')
+  --
+  --   if not status_ok then
+  --     return
+  --   end
+  --   local keymap = vim.keymap.set
+  --
+  --
+  --   lspsaga.setup({
+  --     layout = 'float',
+  --     finder = {
+  --       max_height = 0.6,
+  --       keys = {
+  --         toggle_or_open = '<cr>',
+  --         quit = '<esc>',
+  --       },
+  --     },
+  --   })
+  --   local opts = { noremap = true, silent = true }
+  --
+  --   keymap("n", "gr", "<cmd>Lspsaga finder ref<CR>", opts)
+  --   keymap('n', 'gd', "<cmd>Lspsaga goto_definition<CR>", opts)
+  --   keymap('n', 'K', "<cmd>Lspsaga hover_doc<CR>", opts)
+  -- end
 }
--- lua/diego/plugins/noice.lua
+++ lua/diego/plugins/noice.lua
@@ -1,19 +1,35 @@
 return {
   "folke/noice.nvim",
+  event = "VeryLazy",
   dependencies = {
     "MunifTanjim/nui.nvim",
     "rcarriga/nvim-notify",
   },
   config = function()
-    local noice = require('noice')
+    local noice = require("noice")
+    local notify = require('notify')
+
+    notify.setup({
+      background_colour = '#000000',
+      max_width = 48,
+      stages = "slide",
+      render = "compact",
+    })
+
     noice.setup({
       messages = {
         enabled = false
       },
-      notify = {
-        enabled = false,
-        view = "notify",
-      },
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+        hover = {
+          enabled = false,
+        }
+      }
     })
   end
 }
