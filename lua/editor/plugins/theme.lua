909cb96180b7b827074e84574b9de60caeb1e6a2
Diego Rojas <rrojas1296@gmail.com>
2025-08-04 19:38:56 -0500

    master: Added new theme

--- lua/editor/plugins/theme.lua
+++ lua/editor/plugins/theme.lua
@@ -1,12 +1,13 @@
 return {
-  'marko-cerovac/material.nvim',
+  'catppuccin/nvim',
   lazy = false,
   priority = 1000,
   config = function()
-    local theme = require('material')
+    local theme = require('catppuccin')
     local colorscheme = vim.cmd.colorscheme
-    theme.setup()
-    vim.g.material_style = "deep ocean"
-    colorscheme 'material'
+    theme.setup({
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+    })
+    colorscheme 'catppuccin'
   end
 }
--- lua/editor/snippets/typescriptreact.lua
+++ lua/editor/snippets/typescriptreact.lua
@@ -17,4 +17,29 @@ return {
     rep(1),
     rep(1),
   })),
+  s("rnsafe", fmt([[
+    import {{ StyleSheet, Text }} from "react-native";
+    import {{ SafeAreaView }} from "react-native-safe-area-context";
+
+    const {} = () => {{
+      return <SafeAreaView style={{styles.container}}>
+        <Text>{}</Text>
+      </SafeAreaView>
+    }};
+
+    const styles = StyleSheet.create({{
+      container: {{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff"
+      }}
+    }})
+
+    export default {};
+    ]], {
+    i(1, "ComponentName"),
+    rep(1),
+    rep(1),
+  }))
 }
